192ff23eca313a3ba9428f86011b43ffd406240b
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-06-11 00:53:58 +0200

    Update donazione.js

--- plugins/donazione.js
+++ plugins/donazione.js
@@ -7,9 +7,9 @@ let handler = async (m, { conn }) => {
 
 Se vuoi supportare lo sviluppo del bot e del progetto puoi fare una piccola donazione qui:
 
-👉 https://www.paypal.me/GabWTcomm
+👉 https://www.paypal.me/ElixirBoT85
 
-Grazie per il supporto da parte dello staff di 333 🙏`;
+Grazie per il supporto da parte dello staff di 888 🙏`;
 
   await conn.sendMessage(m.chat, { text });
 };
