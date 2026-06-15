af8a9947b05d63aaddf8cb30118400a2fb583e8a
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-06-15 15:52:55 +0200

    Update fun_afk.js

--- plugins/fun_afk.js
+++ plugins/fun_afk.js
@@ -1,4 +1,5 @@
-// Plugin by Elixir, Punisher & 888 staffimport fs from 'fs'
+// Plugin by Elixir, Punisher & 888 staff
+import fs from 'fs'
 import path from 'path'
 
 const filePath = path.join(process.cwd(), 'data', 'afk.json')
